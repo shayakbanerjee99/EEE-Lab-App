94027d27e0a238d21bc72a9439da6cb50d783eba
shayakbanerjee99 <shayak.banerjee.13@gmail.com>
2018-12-24 19:32:55 +0530

    Updated documentation for Experiment.kt

--- EEELabApp/app/src/main/java/com/example/shayak/eeelabapp/Experiment.kt
+++ EEELabApp/app/src/main/java/com/example/shayak/eeelabapp/Experiment.kt
@@ -1,5 +1,14 @@
 package com.example.shayak.eeelabapp
 
+/**
+ * Experiment contains data about the experiment. It will be used by ExperimentActivity to
+ * load data from the materialLink and will also be used for launching the video in the local
+ * media player.
+ *
+ * @author Shayak Banerjee
+ * @version 1.0
+ * @since 23rd Dec, 2018
+ */
 data class Experiment(val experimentName: String, val materialLink: String, val videoLink: String){
 
 }
